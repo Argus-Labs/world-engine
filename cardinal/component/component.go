39d976143f034e2f06e013e11f3b6e1d8ee9fded
tyler <48813565+technicallyty@users.noreply.github.com>
2023-03-29 16:57:33 -0700

    docs: add godoc to IComponentType interface

--- cardinal/component/component.go
+++ cardinal/component/component.go
@@ -7,9 +7,14 @@ import (
 type (
 	TypeID int
 
+	// IComponentType is a high level representation of a user defined component struct.
 	IComponentType interface {
+		// ID returns the ID of the component.
 		ID() TypeID
+		// New creates a new pointer to the component struct. It will set the struct being pointed to with the default
+		//value if the component was created with one. Otherwise, the struct being pointed to will be empty.
 		New() unsafe.Pointer
+		// Name returns the name of the component.
 		Name() string
 	}
 )
