34e3215359db2e4d6c3a73391123019909cf8706
smsunarto <github@smsunarto.com>
2023-04-05 20:13:51 -0400

    ci: fix lint

--- magefiles/tools.go
+++ magefiles/tools.go
@@ -50,7 +50,7 @@ var (
 	// Dependencies.
 	moq = "github.com/matryer/moq"
 
-	moduleDirs = []string{"contracts", "chain", "magefiles"}
+	moduleDirs = []string{"chain", "magefiles"}
 )
 
 // ===========================================================================
