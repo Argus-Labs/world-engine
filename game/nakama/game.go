7e7175f23fc532c93128b7c33460ea764b0fbccb
tyler <48813565+technicallyty@users.noreply.github.com>
2023-04-20 20:02:46 -0700

    feat: docker compose setup between nakama and cardinal+redis

--- game/nakama/game.go
+++ game/nakama/game.go
@@ -13,9 +13,10 @@ var (
 	StartGameRPC = NewCustomRPC("start-game", func(ctx context.Context, logger runtime.Logger, db *sql.DB, module runtime.NakamaModule, payload string) (string, error) {
 		res, err := cardinal.StartGameLoop(ctx, &ecsv1.MsgStartGameLoop{})
 		if err != nil {
+			logger.Error("there was an error %s", err.Error())
 			return "", err
 		}
-
+		logger.Info("cardinal game loop successfully started.")
 		m := &jsonpb.Marshaler{}
 		return m.MarshalToString(res)
 	})
