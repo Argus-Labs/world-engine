a3f8a1813b735934c01ee713b1012edbacc6ffe5
tyler <48813565+technicallyty@users.noreply.github.com>
2023-05-12 12:31:03 -0700

    feat: proto defs for execution shard implenters

--- chain/router/router.go
+++ chain/router/router.go
@@ -1,6 +1,9 @@
 package router
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type Result struct {
 	Code    uint64
@@ -10,4 +13,27 @@ type Result struct {
 //go:generate mockgen -source=router.go -package mocks -destination mocks/router.go
 type Router interface {
 	Send(ctx context.Context, namespace string, sender string, msg []byte) (Result, error)
+	RegisterNamespace(namespace string, serverAddr string) error
+}
+
+var _ Router = &router{}
+
+type router struct {
+	namespaces map[string]string
+}
+
+func (r *router) Send(ctx context.Context, namespace string, sender string, msg []byte) (Result, error) {
+	addr, ok := r.namespaces[namespace]
+	if !ok {
+		return Result{}, fmt.Errorf("namespace %s not found", namespace)
+	}
+	// connect to client given addr
+	_ = addr
+	// put bytes into proto message and send to server
+	return Result{}, nil
+}
+
+func (r *router) RegisterNamespace(namespace string, serverAddr string) error {
+	r.namespaces[namespace] = serverAddr
+	return nil
 }
