e64a9a6281c324d1efca853d59d04876c382a163
tyler <48813565+technicallyty@users.noreply.github.com>
2023-05-12 18:11:23 -0700

    docs: comment contract interface

--- chain/contracts/src/cosmos/precompile/router.sol
+++ chain/contracts/src/cosmos/precompile/router.sol
@@ -4,7 +4,9 @@ interface IRouter {
     function Send(bytes calldata message, string calldata namespace) external returns (Response memory);
 
     struct Response {
+        // Code is an arbitrary integer that describes the result of execution on an execution shard.
         uint    Code;
+        // Message contains the bytes of an abi encoded struct.
         bytes  Message;
     }
 }
