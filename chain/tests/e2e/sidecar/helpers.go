76b21692c205149a81b30e7752ebb6c485384275
tyler <48813565+technicallyty@users.noreply.github.com>
2023-05-12 17:14:27 -0700

    chore: disable sidecar e2e tests for now'

--- chain/tests/e2e/sidecar/helpers.go
+++ chain/tests/e2e/sidecar/helpers.go
@@ -1,24 +1,25 @@
 package sidecar
 
-import (
-	"testing"
-
-	g1 "buf.build/gen/go/argus-labs/argus/grpc/go/v1/sidecarv1grpc"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"gotest.tools/assert"
-
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-)
-
-func GetBankClient(t *testing.T, url string) banktypes.QueryClient {
-	conn, err := grpc.Dial(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	assert.NilError(t, err)
-	return banktypes.NewQueryClient(conn)
-}
-
-func GetSidecarClient(t *testing.T, url string) g1.SidecarClient {
-	conn, err := grpc.Dial(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	assert.NilError(t, err)
-	return g1.NewSidecarClient(conn)
-}
+//
+//import (
+//	"testing"
+//
+//	g1 "buf.build/gen/go/argus-labs/argus/grpc/go/v1/sidecarv1grpc"
+//	"google.golang.org/grpc"
+//	"google.golang.org/grpc/credentials/insecure"
+//	"gotest.tools/assert"
+//
+//	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+//)
+//
+//func GetBankClient(t *testing.T, url string) banktypes.QueryClient {
+//	conn, err := grpc.Dial(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+//	assert.NilError(t, err)
+//	return banktypes.NewQueryClient(conn)
+//}
+//
+//func GetSidecarClient(t *testing.T, url string) g1.SidecarClient {
+//	conn, err := grpc.Dial(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+//	assert.NilError(t, err)
+//	return g1.NewSidecarClient(conn)
+//}
