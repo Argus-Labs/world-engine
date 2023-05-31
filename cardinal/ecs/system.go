0537e901b291803ad086c065a372a70d03d4fdb3
Jeremy Randolph <jer@argus.gg>
2023-05-31 10:11:28 -0700

    changes interface{} to any

--- cardinal/ecs/system.go
+++ cardinal/ecs/system.go
@@ -3,7 +3,7 @@ package ecs
 // TransactionQueue is a list of transactions that were queued since the start of the
 // last game tick.
 type TransactionQueue struct {
-	queue map[string][]interface{}
+	queue map[string][]any
 }
 
 type System func(*TransactionQueue)
