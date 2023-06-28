1c5eaa16f1f50068137db235428ccc6f5f49458a
tyler <48813565+technicallyty@users.noreply.github.com>
2023-06-28 14:23:28 -0700

    wip: mutex

--- chain/shard/server.go
+++ chain/shard/server.go
@@ -4,6 +4,7 @@ import (
 	shardgrpc "buf.build/gen/go/argus-labs/world-engine/grpc/go/shard/v1/shardv1grpc"
 	shard "buf.build/gen/go/argus-labs/world-engine/protocolbuffers/go/shard/v1"
 	"context"
+	"sync"
 )
 
 var (
@@ -11,9 +12,11 @@ var (
 )
 
 type shardServer struct {
+	lock    sync.Mutex
 	batches [][]byte
 }
 
-func (s shardServer) SubmitShardBatch(ctx context.Context, request *shard.SubmitShardBatchRequest) (*shard.SubmitShardBatchResponse, error) {
+func (s *shardServer) SubmitShardBatch(ctx context.Context, request *shard.SubmitShardBatchRequest) (*shard.SubmitShardBatchResponse, error) {
+
 	return nil, nil
 }
