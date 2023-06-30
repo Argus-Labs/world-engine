eebdc05ea1820e5b13323a1953f7db16c3a1bff2
tyler <48813565+technicallyty@users.noreply.github.com>
2023-06-30 15:14:53 -0700

    test: remove sleep from test, use sync

--- cardinal/ecs/tests/chain_storage_test.go
+++ cardinal/ecs/tests/chain_storage_test.go
@@ -1,17 +1,16 @@
 package tests
 
 import (
+	"context"
 	"encoding/json"
+	"sync"
 	"testing"
-	"time"
 
-	"github.com/golang/mock/gomock"
 	"gotest.tools/v3/assert"
 
-	"github.com/argus-labs/world-engine/cardinal/chain/mocks"
+	"github.com/argus-labs/world-engine/cardinal/chain"
 	"github.com/argus-labs/world-engine/cardinal/ecs"
 	"github.com/argus-labs/world-engine/cardinal/ecs/inmem"
-	"github.com/argus-labs/world-engine/cardinal/ecs/transaction"
 )
 
 type SendEnergy struct {
@@ -19,14 +18,30 @@ type SendEnergy struct {
 	Amount   uint64
 }
 
+var _ chain.Adapter = &MockAdapter{}
+
+type MockAdapter struct {
+	lastSubmittedValue []byte
+	timesCalled        int
+	done               chan int
+}
+
+var sg sync.WaitGroup
+
+func (m *MockAdapter) Submit(ctx context.Context, bz []byte) error {
+	m.lastSubmittedValue = bz
+	m.timesCalled++
+	sg.Done()
+	return nil
+}
+
 func TestWorld_WithChain(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	adapter := mocks.NewMockAdapter(ctrl)
-	w := inmem.NewECSWorldForTest(t, ecs.WithAdapter(adapter))
+	sg = sync.WaitGroup{}
+	mockAdapter := &MockAdapter{}
+	w := inmem.NewECSWorldForTest(t, ecs.WithAdapter(mockAdapter))
 
 	sendEnergyTx := ecs.NewTransactionType[SendEnergy]()
-	txId := transaction.TypeID(1)
-	err := sendEnergyTx.SetID(txId)
+	err := w.RegisterTransactions(sendEnergyTx)
 	assert.NilError(t, err)
 
 	txToSend := SendEnergy{"You", "Me", 400}
@@ -36,7 +51,7 @@ func TestWorld_WithChain(t *testing.T) {
 	})
 
 	expectedBatch := ecs.TxBatch{
-		TxID: txId,
+		TxID: sendEnergyTx.ID(),
 		Txs:  []any{txToSend},
 	}
 	expectedBatches := []ecs.TxBatch{expectedBatch}
@@ -44,16 +59,16 @@ func TestWorld_WithChain(t *testing.T) {
 	bz, err := json.Marshal(expectedBatches)
 	assert.NilError(t, err)
 
-	adapter.EXPECT().Submit(gomock.Any(), bz).Times(1)
-
 	err = w.LoadGameState()
 	assert.NilError(t, err)
 
 	sendEnergyTx.AddToQueue(w, txToSend)
+	sg.Add(1)
 	err = w.Tick()
 	assert.NilError(t, err)
 
-	// sleep to let the go routine finish handling the mock tx submission.
-	// without this, the test returns without ever waiting for the mock to signal that it's done.
-	time.Sleep(time.Millisecond * 1)
+	sg.Wait()
+	assert.Equal(t, mockAdapter.timesCalled, 1)
+	assert.DeepEqual(t, mockAdapter.lastSubmittedValue, bz)
+
 }
