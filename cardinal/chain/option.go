daef844a2ee3dd9ec39542224a27b25bf33fcf72
tyler <48813565+technicallyty@users.noreply.github.com>
2023-07-17 21:59:07 +0200

    panic if creds are not provided within the option

--- cardinal/chain/option.go
+++ cardinal/chain/option.go
@@ -4,6 +4,9 @@ type Option func(adapter *adapterImpl)
 
 func WithCredentials(credPath string) Option {
 	return func(a *adapterImpl) {
+		if credPath == "" {
+			panic("must provide client credential path")
+		}
 		creds, err := loadClientCredentials(credPath)
 		if err != nil {
 			panic(err)
--- cardinal/evm/option.go
+++ cardinal/evm/option.go
@@ -4,6 +4,9 @@ type Option func(*srv)
 
 func WithCredentials(certPath, keyPath string) Option {
 	return func(s *srv) {
+		if certPath == "" || keyPath == "" {
+			panic("must provide both cert and key path")
+		}
 		creds, err := loadCredentials(certPath, keyPath)
 		if err != nil {
 			panic(err)
