9b1c43040578f23d2be860a81bfd1a92706186ef
Tyler <48813565+technicallyty@users.noreply.github.com>
2023-07-19 20:53:33 +0200

    chore(chain): update polaris dep (#170)

--- chain/precompile/router/router.go
+++ chain/precompile/router/router.go
@@ -2,18 +2,12 @@ package router
 
 import (
 	"context"
-	"fmt"
-	"math/big"
-
+	"github.com/argus-labs/world-engine/chain/router"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	"github.com/ethereum/go-ethereum/common"
+	generated "pkg.berachain.dev/polaris/contracts/bindings/cosmos/precompile/router"
 	cosmlib "pkg.berachain.dev/polaris/cosmos/lib"
 	ethprecompile "pkg.berachain.dev/polaris/eth/core/precompile"
-	"pkg.berachain.dev/polaris/lib/utils"
-
-	"github.com/argus-labs/world-engine/chain/precompile"
-	"github.com/argus-labs/world-engine/chain/router"
-	generated "pkg.berachain.dev/polaris/contracts/bindings/cosmos/precompile/router"
+	"pkg.berachain.dev/polaris/eth/core/vm"
 )
 
 const (
@@ -38,48 +32,14 @@ func NewPrecompileContract(r router.Router) ethprecompile.StatefulImpl {
 	}
 }
 
-func (c *Contract) PrecompileMethods() ethprecompile.Methods {
-	return ethprecompile.Methods{
-		{
-			AbiSig:  "Send(bytes,uint64,string)",
-			Execute: c.Send,
-		},
-	}
-}
-
 // Send implements the Send precompile function in router.sol.
 func (c *Contract) Send(
 	ctx context.Context,
-	_ ethprecompile.EVM,
-	caller common.Address,
-	_ *big.Int,
-	_ bool,
-	args ...any,
-) ([]any, error) {
-	if err := matchArgs(maxArgs, len(args)); err != nil {
-		return nil, err
-	}
-	payload, ok := utils.GetAs[[]byte](args[0])
-	if !ok {
-		return nil, precompile.ErrInvalidBytes
-	}
-	msgID, ok := utils.GetAs[uint64](args[1])
-	if !ok {
-		return nil, precompile.ErrInvalidUint64
-	}
-	namespace, ok := utils.GetAs[string](args[2])
-	if !ok {
-		return nil, precompile.ErrInvalidString
-	}
-
-	_, err := c.rtr.Send(ctx, namespace, caller.String(), msgID, payload)
-
-	return []any{}, err
-}
-
-func matchArgs(max, actual int) error {
-	if max != actual {
-		return fmt.Errorf("wanted %d args, got %d", max, actual)
-	}
-	return nil
+	msg []byte,
+	msgID uint64,
+	namespace string,
+) error {
+	pCtx := vm.UnwrapPolarContext(ctx)
+	_, err := c.rtr.Send(ctx, namespace, pCtx.MsgSender().String(), msgID, msg)
+	return err
 }
