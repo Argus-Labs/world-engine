0efb795499ea35cf88ccf35ceee17a38c1ee658c
tyler <48813565+technicallyty@users.noreply.github.com>
2023-08-07 17:37:34 -0700

    refactor: better server impl name

--- cardinal/evm/server.go
+++ cardinal/evm/server.go
@@ -17,7 +17,7 @@ import (
 )
 
 var (
-	_ routerv1grpc.MsgServer = &srv{}
+	_ routerv1grpc.MsgServer = &msgServerImpl{}
 )
 
 // txByID maps transaction type ID's to transaction types.
@@ -26,7 +26,7 @@ type txByID map[transaction.TypeID]transaction.ITransaction
 // readByName maps read resource names to the underlying IRead
 type readByName map[string]ecs.IRead
 
-type srv struct {
+type msgServerImpl struct {
 	txMap      txByID
 	readMap    readByName
 	world      *ecs.World
@@ -50,7 +50,7 @@ func NewServer(w *ecs.World, opts ...Option) (routerv1grpc.MsgServer, error) {
 		ir[r.Name()] = r
 	}
 
-	s := &srv{txMap: it, readMap: ir, world: w}
+	s := &msgServerImpl{txMap: it, readMap: ir, world: w}
 	for _, opt := range opts {
 		opt(s)
 	}
@@ -82,7 +82,7 @@ func loadCredentials(serverCertPath, serverKeyPath string) (credentials.Transpor
 }
 
 // Serve serves the application in a new go routine.
-func (s *srv) Serve(addr string) error {
+func (s *msgServerImpl) Serve(addr string) error {
 	server := grpc.NewServer(s.serverOpts...)
 	routerv1grpc.RegisterMsgServer(server, s)
 	listener, err := net.Listen("tcp", addr)
@@ -98,7 +98,7 @@ func (s *srv) Serve(addr string) error {
 	return nil
 }
 
-func (s *srv) SendMsg(ctx context.Context, msg *routerv1.MsgSend) (*routerv1.MsgSendResponse, error) {
+func (s *msgServerImpl) SendMsg(ctx context.Context, msg *routerv1.MsgSend) (*routerv1.MsgSendResponse, error) {
 	// first we check if we can extract the transaction associated with the id
 	itx, ok := s.txMap[transaction.TypeID(msg.MessageId)]
 	if !ok {
@@ -114,7 +114,7 @@ func (s *srv) SendMsg(ctx context.Context, msg *routerv1.MsgSend) (*routerv1.Msg
 	return &routerv1.MsgSendResponse{}, nil
 }
 
-func (s *srv) QueryShard(ctx context.Context, req *routerv1.QueryShardRequest) (*routerv1.QueryShardResponse, error) {
+func (s *msgServerImpl) QueryShard(ctx context.Context, req *routerv1.QueryShardRequest) (*routerv1.QueryShardResponse, error) {
 	read, ok := s.readMap[req.Resource]
 	if !ok {
 		return nil, fmt.Errorf("no read with name %s found", req.Resource)
