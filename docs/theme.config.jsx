2e078c30a05141df19f21dee1f0cb42227ce379f
smsunarto <github@smsunarto.com>
2023-08-09 16:16:27 -0700

    theming

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,6 +1,30 @@
+import {useRouter} from "next/router";
+
 export default {
-    logo: <span>World Engine</span>,
+    logo: <span>◢ ✦ ◣</span>,
     project: {
         link: 'https://github.com/argus-labs/world-engine',
-    }
+    },
+    footer: {
+        text: (
+            <span>
+        MIT {new Date().getFullYear()} ©{' '}
+                <a href="https://argus.gg" target="_blank">
+          Argus Labs ◢ ✦ ◣
+        </a>
+        .
+      </span>
+        )
+    },
+    useNextSeoProps() {
+        const { asPath } = useRouter()
+        return {
+            titleTemplate: asPath !== "/" ? "%s | World Engine ◢ ✦ ◣" : "Home | World Engine ◢ ✦ ◣",
+            openGraph: {
+                images: [{ url: `/img/og.jpg` }],
+                siteName: "World Engine",
+            },
+        };
+    },
 }
+
