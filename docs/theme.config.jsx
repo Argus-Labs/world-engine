cb62daf8efd654fa99a89266c4ccd2711b802517
smsunarto <github@smsunarto.com>
2023-08-09 16:34:01 -0700

    add favicon

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -10,9 +10,8 @@ export default {
             <span>
         MIT {new Date().getFullYear()} ©{' '}
                 <a href="https://argus.gg" target="_blank">
-          Argus Labs ◢ ✦ ◣
+          ARGUS ◢ ✦ ◣
         </a>
-        .
       </span>
         )
     },
@@ -26,5 +25,15 @@ export default {
             },
         };
     },
+    head: (
+        <>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f5d64e" />
+            <meta name="msapplication-TileColor" content="#da532c"/ >
+            <meta name="theme-color" content="#fffce4" />
+        </>
+    )
 }
-
