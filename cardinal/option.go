1758bcda2c6f26ef4bb91495a65bd11f81991ba1
Jeremy Randolph <jer@argus.gg>
2023-09-06 12:19:03 -0700

    Include soon-to-be-deprecated endpoints that are prefixed with "read"

--- cardinal/option.go
+++ cardinal/option.go
@@ -31,6 +31,12 @@ func WithReceiptHistorySize(size int) WorldOption {
 	}
 }
 
+func WithPrettyLog() WorldOption {
+	return WorldOption{
+		ecsOption: ecs.WithPrettyLog(),
+	}
+}
+
 // WithNamespace sets the World's namespace. The default is "world". The namespace is used in the transaction
 // signing process.
 func WithNamespace(namespace string) WorldOption {
