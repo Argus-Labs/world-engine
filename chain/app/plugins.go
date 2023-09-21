7e79e00dfb14c0db60eea6eb9228b3e9caff569a
Tyler <48813565+technicallyty@users.noreply.github.com>
2023-09-21 13:54:26 -0700

    refactor(chain): defaults for shard sequencer (#287)

--- chain/app/plugins.go
+++ chain/app/plugins.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"github.com/rs/zerolog/log"
 	"os"
 	"pkg.world.dev/world-engine/chain/router"
 	"pkg.world.dev/world-engine/chain/shard"
@@ -8,17 +9,22 @@ import (
 
 func (app *App) setPlugins() {
 	// TODO: clean this up. maybe a config?
-	shardHandlerListener := os.Getenv("SHARD_HANDLER_LISTEN_ADDR")
-	if shardHandlerListener != "" {
-		certPath := os.Getenv("SERVER_CERT_PATH")
-		keyPath := os.Getenv("SERVER_KEY_PATH")
-		app.ShardHandler = shard.NewShardServer(shard.WithCredentials(certPath, keyPath))
-		app.ShardHandler.Serve(shardHandlerListener)
+	certPath := os.Getenv("SERVER_CERT_PATH")
+	keyPath := os.Getenv("SERVER_KEY_PATH")
+	if certPath == "" || keyPath == "" {
+		log.Warn().Msg("running shard sequencer without SSL certs")
+		app.ShardSequencer = shard.NewShardSequencer()
+	} else {
+		app.ShardSequencer = shard.NewShardSequencer(shard.WithCredentials(certPath, keyPath))
 	}
+
+	app.ShardSequencer.Serve()
 
 	cardinalShardAddr := os.Getenv("CARDINAL_EVM_LISTENER_ADDR")
 	if cardinalShardAddr != "" {
 		clientCert := os.Getenv("CLIENT_CERT_PATH")
 		app.Router = router.NewRouter(cardinalShardAddr, router.WithCredentials(clientCert))
+	} else {
+		log.Warn().Msg("router is not running")
 	}
 }
