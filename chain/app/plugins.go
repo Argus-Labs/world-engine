e992b98ca01875cd91c0da5816f9821e6d8bac9d
tyler <48813565+technicallyty@users.noreply.github.com>
2023-09-21 12:44:31 -0700

    style: more explicit sequencer instantiation

--- chain/app/plugins.go
+++ chain/app/plugins.go
@@ -11,13 +11,13 @@ func (app *App) setPlugins() {
 	// TODO: clean this up. maybe a config?
 	certPath := os.Getenv("SERVER_CERT_PATH")
 	keyPath := os.Getenv("SERVER_KEY_PATH")
-	var opt shard.Option
-	if certPath == "" && keyPath == "" {
+	if certPath == "" || keyPath == "" {
 		log.Warn().Msg("running shard sequencer without SSL certs")
+		app.ShardSequencer = shard.NewShardSequencer()
 	} else {
-		opt = shard.WithCredentials(certPath, keyPath)
+		app.ShardSequencer = shard.NewShardSequencer(shard.WithCredentials(certPath, keyPath))
 	}
-	app.ShardSequencer = shard.NewShardSequencer(opt)
+
 	app.ShardSequencer.Serve()
 
 	cardinalShardAddr := os.Getenv("CARDINAL_EVM_LISTENER_ADDR")
