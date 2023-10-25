ee8ee1773b663db711e4439971862ebb6731f63f
jerargus <132295941+jerargus@users.noreply.github.com>
2023-10-25 16:48:53 -0700

    test(cardinal): Re-add cardinal package tests and add helper function to test_utils t… (#355)

--- cardinal/ecs/mock_world.go
+++ cardinal/ecs/mock_world.go
@@ -2,35 +2,37 @@ package ecs
 
 import (
 	"fmt"
-	"log"
 	"testing"
 
-	"gotest.tools/v3/assert"
-
 	"github.com/alicebob/miniredis/v2"
+	"github.com/rs/zerolog/log"
+	"gotest.tools/v3/assert"
 	"pkg.world.dev/world-engine/cardinal/ecs/ecb"
-
 	"pkg.world.dev/world-engine/cardinal/ecs/storage"
 )
 
 // NewMockWorld creates an ecs.World that uses a mock redis DB as the storage
 // layer. This is only suitable for local development. If you are creating an ecs.World for
 // unit tests, use NewTestWorld.
-func NewMockWorld(opts ...Option) *World {
+func NewMockWorld(opts ...Option) (world *World, cleanup func()) {
 	// We manually set the start address to make the port deterministic
 	s := miniredis.NewMiniRedis()
 	err := s.StartAddr(":12345")
 	if err != nil {
 		panic("Unable to initialize in-memory redis")
 	}
-	log.Printf("Miniredis started at %s", s.Addr())
+	log.Logger.Debug().Msgf("miniredis started at %s", s.Addr())
 
 	w, err := newMockWorld(s, opts...)
 	if err != nil {
 		panic(fmt.Errorf("unable to initialize world: %w", err))
 	}
 
-	return w
+	return w, func() {
+		log.Logger.Debug().Msg("miniredis shutting down")
+		s.Close()
+		log.Logger.Debug().Msg("successfully shut down miniredis")
+	}
 }
 
 // NewTestWorld creates an ecs.World suitable for running in tests. Relevant resources
---
+++ cardinal/query_test.go
@@ -0,0 +1,74 @@
+package cardinal_test
+
+import (
+	"testing"
+
+	"gotest.tools/v3/assert"
+	"pkg.world.dev/world-engine/cardinal"
+	"pkg.world.dev/world-engine/cardinal/test_utils"
+)
+
+type QueryHealthRequest struct {
+	Min int
+}
+
+type QueryHealthResponse struct {
+	IDs []cardinal.EntityID
+}
+
+func handleQueryHealth(worldCtx cardinal.WorldContext, request *QueryHealthRequest) (*QueryHealthResponse, error) {
+	q, err := worldCtx.NewSearch(cardinal.Exact(Health{}))
+	if err != nil {
+		return nil, err
+	}
+	resp := &QueryHealthResponse{}
+	err = q.Each(worldCtx, func(id cardinal.EntityID) bool {
+		health, err := cardinal.GetComponent[Health](worldCtx, id)
+		if err != nil {
+			return true
+		}
+		if health.Value < request.Min {
+			return true
+		}
+		resp.IDs = append(resp.IDs, id)
+		return true
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+var queryHealth = cardinal.NewQueryType[*QueryHealthRequest, *QueryHealthResponse]("query_health", handleQueryHealth)
+
+func TestQueryExample(t *testing.T) {
+	world, _ := test_utils.MakeWorldAndTicker(t)
+	assert.NilError(t, cardinal.RegisterComponent[Health](world))
+	assert.NilError(t, cardinal.RegisterQueries(world, queryHealth))
+
+	worldCtx := test_utils.WorldToWorldContext(world)
+	ids, err := cardinal.CreateMany(worldCtx, 100, Health{})
+	assert.NilError(t, err)
+	// Give each new entity health based on the ever-increasing index
+	for i, id := range ids {
+		assert.NilError(t, cardinal.UpdateComponent[Health](worldCtx, id, func(h *Health) *Health {
+			h.Value = i
+			return h
+		}))
+	}
+
+	// No entities should have health over a million.
+	resp, err := queryHealth.DoQuery(worldCtx, &QueryHealthRequest{1_000_000})
+	assert.NilError(t, err)
+	assert.Equal(t, 0, len(resp.IDs))
+
+	// All entities should have health over -100
+	resp, err = queryHealth.DoQuery(worldCtx, &QueryHealthRequest{-100})
+	assert.NilError(t, err)
+	assert.Equal(t, 100, len(resp.IDs))
+
+	// Exactly 10 entities should have health at or above 90
+	resp, err = queryHealth.DoQuery(worldCtx, &QueryHealthRequest{90})
+	assert.NilError(t, err)
+	assert.Equal(t, 10, len(resp.IDs))
+}
