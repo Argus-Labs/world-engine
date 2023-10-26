90db26872f65c1339cb3fe30d862a80a271ceac4
Namra Patel <namrajpatel@gmail.com>
2023-10-26 13:56:28 -0700

    chore(cardinal): MakeWorldAndTicker accepts options (#363)

--- cardinal/test_utils/test_utils.go
+++ cardinal/test_utils/test_utils.go
@@ -169,11 +169,10 @@ func AddTransactionToWorldByAnyTransaction(world *cardinal.World, cardinalTx car
 // MakeWorldAndTicker sets up a cardinal.World as well as a function that can execute one game tick. The *cardinal.World
 // will be automatically started when doTick is called for the first time. The cardinal.World will be shut down at the
 // end of the test. If doTick takes longer than 5 seconds to run, t.Fatal will be called.
-func MakeWorldAndTicker(t *testing.T) (world *cardinal.World, doTick func()) {
+func MakeWorldAndTicker(t *testing.T, opts ...cardinal.WorldOption) (world *cardinal.World, doTick func()) {
 	startTickCh, doneTickCh := make(chan time.Time), make(chan uint64)
-	world, err := cardinal.NewMockWorld(
-		cardinal.WithTickChannel(startTickCh),
-		cardinal.WithTickDoneChannel(doneTickCh))
+	opts = append(opts, cardinal.WithTickChannel(startTickCh), cardinal.WithTickDoneChannel(doneTickCh))
+	world, err := cardinal.NewMockWorld(opts...)
 	if err != nil {
 		t.Fatalf("unable to make mock world: %v", err)
 	}
