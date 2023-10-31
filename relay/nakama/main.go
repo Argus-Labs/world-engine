8d4c8b52092a7f1f1908310ca3654d51b6dcad09
anon <1806857+pyrofolium@users.noreply.github.com>
2023-10-30 20:45:34 -0700

    refactor(relay): use event hub methods (#371)

--- relay/nakama/main.go
+++ relay/nakama/main.go
@@ -132,7 +132,7 @@ func initEventHub(ctx context.Context, log runtime.Logger, nk runtime.NakamaModu
 		return err
 	}
 	go func() {
-		err := eventHub.dispatch(log)
+		err := eventHub.Dispatch(log)
 		if err != nil {
 			log.Error("error initializing eventHub: %s", err.Error())
 		}
@@ -140,7 +140,7 @@ func initEventHub(ctx context.Context, log runtime.Logger, nk runtime.NakamaModu
 
 	//for now send to everybody via notifications.
 	go func() {
-		channel := eventHub.subscribe("main")
+		channel := eventHub.Subscribe("main")
 		for event := range channel {
 			err := nk.NotificationSendAll(ctx, "event", map[string]interface{}{"message": event.message}, 1, true)
 			if err != nil {
