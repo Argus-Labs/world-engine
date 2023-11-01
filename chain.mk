163cbe86fbc4c6f492dd4991d1f5b1f5b34328c9
heronimus <adie@heronimus.id>
2023-11-01 22:45:03 +0700

    chore(ci): fix rollup build only makefile

--- chain.mk
+++ chain.mk
@@ -12,4 +12,4 @@ forge-build: |
 	@forge build --extra-output-files bin --extra-output-files abi  --root chain/contracts
 
 rollup-build:
-	cd chain && docker compose up game nakama
+	cd chain && docker compose build
