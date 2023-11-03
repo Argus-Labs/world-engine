090226135ba8e84439fa257e5d22a86dbf094c29
tyler <48813565+technicallyty@users.noreply.github.com>
2023-11-03 13:08:21 -0700

    okay dont vendor i guess

--- chain.mk
+++ chain.mk
@@ -3,7 +3,6 @@ rift:
 .PHONY: rift
 
 rollup:
-	cd chain && go mod vendor
 	./chain/scripts/start.sh --build
 
 game:
@@ -13,5 +12,4 @@ forge-build: |
 	@forge build --extra-output-files bin --extra-output-files abi  --root chain/precompile/contracts
 
 rollup-build:
-	cd chain && go mod vendor
-	cd chain && docker compose build
+	@docker build chain
