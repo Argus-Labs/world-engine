9010aa26a963345354652afa3f6175de77e0039b
smsunarto <github@smsunarto.com>
2023-11-10 20:27:35 -0800

    whoops

--- cardinal/testutils/test_utils.go
+++ cardinal/testutils/test_utils.go
@@ -7,7 +7,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/rs/zerolog"
 	"net/http"
 	"sync"
 	"testing"
@@ -24,7 +23,6 @@ import (
 )
 
 func MakeTestTransactionHandler(t *testing.T, world *ecs.World, opts ...server.Option) *TestTransactionHandler {
-	zerolog.SetGlobalLevel(zerolog.WarnLevel)
 	port := "4040"
 	opts = append(opts, server.WithPort(port))
 	eventHub := events.CreateWebSocketEventHub()
