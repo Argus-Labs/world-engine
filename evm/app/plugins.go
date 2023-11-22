e678588d8450aad90d42b02a63abc83d8a3e22e3
tyler <48813565+technicallyty@users.noreply.github.com>
2023-11-22 11:29:19 -0800

    remove conditional router start

--- evm/app/plugins.go
+++ evm/app/plugins.go
@@ -9,7 +9,6 @@ import (
 )
 
 func (app *App) setPlugins(logger log.Logger) {
-	// TODO: clean this up. maybe a config?
 	certPath := os.Getenv("SERVER_CERT_PATH")
 	keyPath := os.Getenv("SERVER_KEY_PATH")
 	if certPath == "" || keyPath == "" {
@@ -22,20 +21,14 @@ func (app *App) setPlugins(logger log.Logger) {
 
 	app.ShardSequencer.Serve()
 
-	// TODO: we dont need cardinal addr anymore. we're gonna get it from state machine.
-	cardinalShardAddr := os.Getenv("CARDINAL_EVM_LISTENER_ADDR")
-	if cardinalShardAddr != "" {
-		var opts []router.Option
-		clientCert := os.Getenv("CLIENT_CERT_PATH")
-		if clientCert != "" {
-			logger.Info("running router client with client certification")
-			opts = append(opts, router.WithCredentials(clientCert))
-		} else {
-			logger.Info("WARNING: running router client without client certification. this will cause issues if " +
-				"the cardinal instance uses SSL credentials")
-		}
-		app.Router = router.NewRouter(logger, app.CreateQueryContext, app.NamespaceKeeper.Address, opts...)
+	var opts []router.Option
+	clientCert := os.Getenv("CLIENT_CERT_PATH")
+	if clientCert != "" {
+		logger.Info("running router with client certification")
+		opts = append(opts, router.WithCredentials(clientCert))
 	} else {
-		logger.Info("router is not running")
+		logger.Error("WARNING: running router client without client certification. this will cause issues if " +
+			"the game shard instance uses SSL credentials")
 	}
+	app.Router = router.NewRouter(logger, app.CreateQueryContext, app.NamespaceKeeper.Address, opts...)
 }
