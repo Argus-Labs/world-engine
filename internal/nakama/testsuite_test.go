eff5eca0d6f25ed92bec680dd5b3f73b5980decc
tyler <48813565+technicallyty@users.noreply.github.com>
2024-01-19 19:20:15 -0800

    skip events test

--- internal/nakama/testsuite_test.go
+++ internal/nakama/testsuite_test.go
@@ -17,6 +17,7 @@ import (
 )
 
 func TestEvents(t *testing.T) {
+	t.Skip("skipping this test til events are integrated")
 	// Note if this test is failing it could be because redis is not refreshed
 	// This test assumes that your redis is brand new and empty.
 	// Test persona
