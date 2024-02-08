1b5baa18be8180340ddff7a0e0d2b8c990d72d5b
Brian Yeh <brian@argus.gg>
2024-02-08 14:59:56 -0800

    remove comments.

--- e2e/testgames/gamebenchmark/main.go
+++ e2e/testgames/gamebenchmark/main.go
@@ -71,31 +71,7 @@ func main() {
 		panic(eris.ToString(err, true))
 	}
 
-	// TODO: Unused decorator somehow this breaks profiling (it ends up with missing pieces of the stack).
-	// TODO: Figure out how to get this to work.
-	// timedShutdownDecorator := func(system cardinal.System, tickAmount int) cardinal.System {
-	//	counter := 0
-	//	return func(context cardinal.WorldContext) error {
-	//		context.Engine().GetEngine().Logger.Info().Msg("system")
-	//		if counter >= tickAmount {
-	//			pid := os.Getpid()                        // Get the current process's PID
-	//			err := syscall.Kill(pid, syscall.SIGTERM) // Send SIGTERM to itself
-	//			if err != nil {
-	//				panic(err) // Handle error
-	//			}
-	//			return nil
-	//		} else {
-	//			counter++
-	//			return system(context)
-	//		}
-	//	}
-	//}
-
 	initsystems, systems = initializeSystems(initsystems, systems)
-
-	// for i, system := range systems {
-	//	systems[i] = timedShutdownDecorator(system, 100)
-	// }
 
 	err = cardinal.RegisterSystems(world, systems...)
 	if err != nil {
