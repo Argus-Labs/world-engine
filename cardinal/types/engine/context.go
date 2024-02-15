a72f52cd6000ec219a9762dba4cab625738dca45
Scott Sunarto <github@smsunarto.com>
2024-02-14 19:23:15 -0800

    add comments to context external method

--- cardinal/types/engine/context.go
+++ cardinal/types/engine/context.go
@@ -10,18 +10,23 @@ import (
 )
 
 type Context interface {
+	// Timestamp returns the UNIX timestamp of the tick.
 	Timestamp() uint64
+	// CurrentTick returns the current tick.
 	CurrentTick() uint64
+	// Logger returns the logger that can be used to log messages from within system or query.
 	Logger() *zerolog.Logger
-	AddMessageError(id types.TxHash, err error)
-	SetMessageResult(id types.TxHash, a any)
+	// EmitEvent emits an event that will be broadcasted to all websocket subscribers.
 	EmitEvent(string)
+	// Namespace returns the namespace of the world.
 	Namespace() string
 
 	// For internal use.
 
 	// SetLogger is used to inject a new logger configuration to an engine context that is already created.
 	SetLogger(logger zerolog.Logger)
+	AddMessageError(id types.TxHash, err error)
+	SetMessageResult(id types.TxHash, a any)
 	GetComponentByName(name string) (types.ComponentMetadata, error)
 	GetTransactionReceipt(id types.TxHash) (any, []error, bool)
 	GetSignerForPersonaTag(personaTag string, tick uint64) (addr string, err error)
