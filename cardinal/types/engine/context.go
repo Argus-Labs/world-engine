14ac8876b925908cc317eed7447b288120382b6a
Scott Sunarto <github@smsunarto.com>
2024-02-14 19:20:35 -0800

    separate out interna and external method in context

--- cardinal/types/engine/context.go
+++ cardinal/types/engine/context.go
@@ -13,22 +13,23 @@ type Context interface {
 	Timestamp() uint64
 	CurrentTick() uint64
 	Logger() *zerolog.Logger
+	AddMessageError(id types.TxHash, err error)
+	SetMessageResult(id types.TxHash, a any)
+	EmitEvent(string)
+	Namespace() string
+
+	// For internal use.
+
 	// SetLogger is used to inject a new logger configuration to an engine context that is already created.
 	SetLogger(logger zerolog.Logger)
 	GetComponentByName(name string) (types.ComponentMetadata, error)
-	AddMessageError(id types.TxHash, err error)
-	SetMessageResult(id types.TxHash, a any)
 	GetTransactionReceipt(id types.TxHash) (any, []error, bool)
-	EmitEvent(string)
 	GetSignerForPersonaTag(personaTag string, tick uint64) (addr string, err error)
 	GetTransactionReceiptsForTick(tick uint64) ([]receipt.Receipt, error)
 	ReceiptHistorySize() uint64
-	Namespace() string
 	AddTransaction(id types.MessageID, v any, sig *sign.Transaction) (uint64, types.TxHash)
 	UseNonce(signerAddress string, nonce uint64) error
 	IsWorldReady() bool
-
-	// For internal use.
 	StoreReader() gamestate.Reader
 	StoreManager() gamestate.Manager
 	GetTxQueue() *txpool.TxQueue
