b4bad5ae5ee26eca7a1b6db491289b04c845bacb
namrapatel <namrajpatel@gmail.com>
2024-02-21 17:11:00 -0800

    caps

--- cardinal/worldstage/worldstage.go
+++ cardinal/worldstage/worldstage.go
@@ -9,7 +9,7 @@ type Stage string
 const (
 	Init         Stage = "Init"         // The default stage of world
 	Starting     Stage = "Starting"     // World is moved to this stage after StartGame() is called
-	Recovering   Stage = "Recovering"   // World is moved to this stage when recoverFromChain() is called
+	Recovering   Stage = "Recovering"   // World is moved to this stage when RecoverFromChain() is called
 	Ready        Stage = "Ready"        // World is moved to this stage when it's ready to start ticking
 	Running      Stage = "Running"      // World is moved to this stage when Tick() is first called
 	ShuttingDown Stage = "ShuttingDown" // World is moved to this stage when it received a shutdown signal
