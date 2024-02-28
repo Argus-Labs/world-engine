deda2b5a890daebaafc604c1d9010b1fba5ed7c2
tyler <48813565+technicallyty@users.noreply.github.com>
2024-02-28 13:52:56 -0800

    add simple e2e test

--- e2e/chain/chain_client.go
+++ e2e/chain/chain_client.go
@@ -8,16 +8,22 @@ import (
 	"gotest.tools/v3/assert"
 
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	namespacetypes "pkg.world.dev/world-engine/evm/x/namespace/types"
 	shardtypes "pkg.world.dev/world-engine/evm/x/shard/types"
 )
 
 type Chain struct {
-	shard shardtypes.QueryClient
-	bank  banktypes.QueryClient
+	shard     shardtypes.QueryClient
+	bank      banktypes.QueryClient
+	namespace namespacetypes.QueryServiceClient
 }
 
 func newChainClient(t *testing.T) Chain {
 	cc, err := grpc.Dial("localhost:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	assert.NilError(t, err)
-	return Chain{shard: shardtypes.NewQueryClient(cc), bank: banktypes.NewQueryClient(cc)}
+	return Chain{
+		shard:     shardtypes.NewQueryClient(cc),
+		bank:      banktypes.NewQueryClient(cc),
+		namespace: namespacetypes.NewQueryServiceClient(cc),
+	}
 }
