0fe3d7cc134fdfda265e17b72b24ff5f160c8a80
namrapatel <namrajpatel@gmail.com>
2024-03-05 13:05:42 -0800

    new attempt

--- makefiles/test.mk
+++ makefiles/test.mk
@@ -12,8 +12,8 @@ e2e-nakama:
 		cd $(ROOT_DIR); \
 	)
 
-	@docker compose up --build game nakama test_nakama --abort-on-container-exit --exit-code-from test_nakama --attach test_nakama
-	@docker compose down -v
+	@docker compose --project-name e2e up --build game nakama test_nakama --abort-on-container-exit --exit-code-from test_nakama --attach test_nakama
+	@docker compose --project-name e2e down -v --remove-orphans --timeout 0
 
 e2e-benchmark:
 	$(foreach dir, $(DIRS_E2E_BENCHMARK), \
