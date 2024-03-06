923b9adda3714747f441695518b38daf1e58fb98
namrapatel <namrajpatel@gmail.com>
2024-03-05 16:25:26 -0800

    grep

--- makefiles/test.mk
+++ makefiles/test.mk
@@ -12,7 +12,7 @@ e2e-nakama:
 		cd $(ROOT_DIR); \
 	)
 
-	@docker compose up --build game nakama test_nakama --abort-on-container-exit --exit-code-from test_nakama
+	@docker compose up --build game nakama test_nakama --abort-on-container-exit --exit-code-from test_nakama 2>&1 | grep test_nakama
 	@docker compose down --volumes -v
 
 e2e-benchmark:
