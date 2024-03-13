00fafc2d584d3beaf6beed7eec7670147dd731f3
Jeremy Randolph <jer@argus.gg>
2024-03-12 17:20:33 -0700

    Use sliding window to track nonces

--- cardinal/storage/nonce_test.go
+++ cardinal/storage/nonce_test.go
@@ -1,13 +1,15 @@
 package storage_test
 
 import (
+	"context"
 	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/alicebob/miniredis/v2"
-	"pkg.world.dev/world-engine/cardinal/storage/redis"
-
 	"pkg.world.dev/world-engine/assert"
+
+	"pkg.world.dev/world-engine/cardinal/storage/redis"
 )
 
 const Namespace string = "world"
@@ -38,6 +40,96 @@ func TestCanStoreManyNonces(t *testing.T) {
 	for i := uint64(10); i < 100; i++ {
 		addr := fmt.Sprintf("%d", i)
 		err := rs.UseNonce(addr, i)
+		assert.ErrorIs(t, redis.ErrNonceHasAlreadyBeenUsed, err)
+	}
+}
+
+func TestNonceStorageIsBounded(t *testing.T) {
+	rs := GetRedisStorage(t)
+	addr := "some-address"
+	// totalNonceCount is the total number of nonces we want to "use"
+	totalNonceCount := 3 * redis.NonceSlidingWindowSize
+	// maxNonceSaved is the maximum number of nonces we should expect to see at the end of this test. If this value is
+	// exceeded it likely means the storage for nonces is unbounded. We don't want to set this limit to *exactly*
+	// NonceSlidingWindowSize because we want the nonce manager to have some leeway as to when exactly it will prune
+	// old nonces.
+	maxNonceSaved := 2 * redis.NonceSlidingWindowSize
+	for i := 0; i < totalNonceCount; i++ {
+		assert.NilError(t, rs.UseNonce(addr, uint64(i)), "using nonce %v failed", i)
+	}
+
+	// Find the redis key that contains the used nonces for the above address
+	client := rs.Client
+	ctx := context.Background()
+	keys, err := client.Keys(ctx, "*"+addr+"*").Result()
+	assert.NilError(t, err)
+	assert.Equal(t, 1, len(keys))
+	count, err := client.ZCard(ctx, keys[0]).Result()
+	assert.NilError(t, err)
+	assert.Check(t, count < int64(maxNonceSaved), "nonce tracking is unbounded")
+}
+
+func TestOutOfOrderNoncesAreOK(t *testing.T) {
+	count := 100
+	nonces := make([]uint64, count)
+	for i := range nonces {
+		nonces[i] = uint64(i)
+	}
+	r := rand.New(rand.NewSource(0))
+	r.Shuffle(count, func(i, j int) {
+		nonces[i], nonces[j] = nonces[j], nonces[i]
+	})
+
+	rs := GetRedisStorage(t)
+	for _, n := range nonces {
+		assert.NilError(t, rs.UseNonce("some-addr", n))
+	}
+	m := map[int]int{}
+	clear(m)
+}
+
+// TestCannotReuseNonceAfterPrune ensures off-by-one errors related to pruning nonces outside the NonceSlidingWindowSize
+// do not result in the ability to reuse an already-used nonce.
+func TestCannotReuseNonceAfterPrune(t *testing.T) {
+	rs := GetRedisStorage(t)
+	total := 3 * redis.NonceSlidingWindowSize
+	addr := "some-addr"
+	for i := 0; i < total; i++ {
+		assert.NilError(t, rs.UseNonce(addr, uint64(i)))
+		if i > 10 {
+			err := rs.UseNonce(addr, uint64(i-10))
+			assert.ErrorIs(t, redis.ErrNonceHasAlreadyBeenUsed, err)
+		}
+		if i > redis.NonceSlidingWindowSize+1 {
+			alreadyUsed := uint64(i - redis.NonceSlidingWindowSize)
+			before := alreadyUsed - 1
+			after := alreadyUsed + 1
+
+			// Make sure the nonces around the sliding window size always return an error
+			assert.IsError(t, rs.UseNonce(addr, before), "%d was already used", before)
+			assert.IsError(t, rs.UseNonce(addr, alreadyUsed), "%d was already used", alreadyUsed)
+			assert.IsError(t, rs.UseNonce(addr, after), "%d was already used", after)
+		}
+	}
+}
+
+func TestUsedNoncesAreRememberedAcrossRestart(t *testing.T) {
+	s := miniredis.RunT(t)
+	opts := redis.Options{
+		Addr:     s.Addr(),
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	}
+	rsOne := redis.NewRedisStorage(opts, Namespace)
+
+	addr := "some-addr"
+	for i := 0; i < 10; i++ {
+		assert.NilError(t, rsOne.UseNonce(addr, uint64(i)))
+	}
+
+	rsTwo := redis.NewRedisStorage(opts, Namespace)
+	for i := 0; i < 10; i++ {
+		err := rsTwo.UseNonce(addr, uint64(i))
 		assert.ErrorIs(t, redis.ErrNonceHasAlreadyBeenUsed, err)
 	}
 }
--- cardinal/storage/redis/nonce.go
+++ cardinal/storage/redis/nonce.go
@@ -3,35 +3,118 @@ package redis
 import (
 	"context"
 	"errors"
+	"fmt"
+	"strconv"
+	"sync"
 
 	"github.com/redis/go-redis/v9"
 	"github.com/rotisserie/eris"
+	"github.com/rs/zerolog/log"
 )
+
+const (
+	NonceSlidingWindowSize = 1000
+
+	// maxValidNonce is the largest nonce that is guaranteed to have a unique ZSet score from all smaller nonces.
+	// A ZSet in redis is used to track unique nonces. Each item in a ZSet has a score, which is stored as a float64.
+	// Due to the precision loss when converting large integers to floating point numbers, at some point 2 distinct
+	// nonces will map to the same score in the Redis ZSet.
+	maxValidNonce       = (1 << (float64MantissaSize + 1)) - 1
+	float64MantissaSize = 52
+)
+
+var ErrNonceHasAlreadyBeenUsed = errors.New("nonce has already been used")
 
 type NonceStorage struct {
 	Client *redis.Client
+	// mutex locks the UseNonce function to make it safe for concurrent access. This is a single lock for all signer
+	// addresses. An improvement on NonceStorage would have a different lock for each signer addresses.
+	mutex      *sync.Mutex
+	maxNonce   map[string]uint64
+	countNonce map[string]int
 }
 
 func NewNonceStorage(client *redis.Client) NonceStorage {
 	return NonceStorage{
-		Client: client,
+		Client:     client,
+		mutex:      &sync.Mutex{},
+		maxNonce:   map[string]uint64{},
+		countNonce: map[string]int{},
 	}
 }
-
-var ErrNonceHasAlreadyBeenUsed = errors.New("nonce has already been used")
 
 // UseNonce atomically marks the given nonce as used. The nonce is valid if nil is returned. A non-nil error means
 // there was an error verifying the nonce, or the nonce was already used.
 func (r *NonceStorage) UseNonce(signerAddress string, nonce uint64) error {
+	if nonce > maxValidNonce {
+		return eris.New("nonce is too large")
+	}
 	ctx := context.Background()
 	key := r.nonceSetKey(signerAddress)
-	added, err := r.Client.SAdd(ctx, key, nonce).Result()
+
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	maxNonce, err := r.getMaxNonceForKey(ctx, key)
 	if err != nil {
-		return err
+		return eris.Wrap(err, "failed to get max nonce for signer address")
 	}
-	// The nonce was already used
+
+	if nonce < maxNonce && maxNonce-nonce >= NonceSlidingWindowSize {
+		return eris.New("nonce is too old")
+	}
+
+	zItem := redis.Z{
+		Score:  float64(nonce),
+		Member: nonce,
+	}
+
+	// This test assumes we're using redis
+	added, err := r.Client.ZAdd(ctx, key, zItem).Result()
+	if err != nil {
+		return eris.Wrap(err, "failed to add nonce")
+	}
 	if added == 0 {
 		return eris.Wrapf(ErrNonceHasAlreadyBeenUsed, "signer %q has already used nonce %d", signerAddress, nonce)
 	}
+
+	r.maxNonce[key] = max(r.maxNonce[key], nonce)
+	r.countNonce[key]++
+
+	if r.countNonce[key] > 1.5*NonceSlidingWindowSize {
+		r.pruneOldNonces(ctx, key, r.maxNonce[key])
+	}
+
 	return nil
+}
+
+func (r *NonceStorage) pruneOldNonces(ctx context.Context, key string, currMax uint64) {
+	minScore := "-inf"
+	maxScore := fmt.Sprintf("%d", currMax-NonceSlidingWindowSize)
+	removed, err := r.Client.ZRemRangeByScore(ctx, key, minScore, maxScore).Result()
+	if err != nil {
+		log.Err(err).Msg("failed to remove some old nonces")
+		return
+	}
+	r.countNonce[key] -= int(removed)
+}
+
+func (r *NonceStorage) getMaxNonceForKey(ctx context.Context, key string) (uint64, error) {
+	maxNonce, ok := r.maxNonce[key]
+	if ok {
+		return maxNonce, nil
+	}
+	values, err := r.Client.ZRange(ctx, key, -1, 0).Result()
+	if err != nil {
+		return 0, eris.Wrap(err, "failed to get range of nonce values")
+	}
+	if len(values) > 0 {
+		maxNonce, err = strconv.ParseUint(values[0], 10, 64)
+		if err != nil {
+			return 0, eris.Wrapf(err, "failed to convert %q to uint64", values[0])
+		}
+	}
+	// if len(values) == 0, no nonce has been saved for this key
+	r.maxNonce[key] = maxNonce
+	return maxNonce, nil
 }
