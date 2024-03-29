43bd1259f402ccd784b5de7229a6e5703ab11fe6
Brian Yeh <brian@argus.gg>
2024-03-29 10:27:04 -0700

    various fixes and touchups.

--- cardinal/search/searchcomponent.go
+++ cardinal/search/searchcomponent.go
@@ -5,12 +5,16 @@ import "pkg.world.dev/world-engine/cardinal/types"
 // This file represents primitives for search
 // The primitive wraps the component and is used in search
 // the purpose of the wrapper is to prevent the user from ever
-// instantiating the component.
+// instantiating the component. These wrappers are used to check if entities
+// contain the specified component during the search.
 
 type componentWrapper struct {
 	Component types.Component
 }
 
+// Component is public but contains an unexported return type
+// this is done with intent as the user should never use componentWrapper
+// explicitly.
 func Component[T types.Component]() componentWrapper {
 	var x T
 	return componentWrapper{
