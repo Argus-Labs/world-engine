c565f05cdfd6416f6f47a71c68a6302850e24760
tyler <48813565+technicallyty@users.noreply.github.com>
2024-04-03 16:32:12 -0700

    add rift client, ensure entities can be created during recovery

--- e2e/tests/clients/evm.go
+++ e2e/tests/clients/evm.go
@@ -10,6 +10,8 @@ import (
 
 	namespacetypes "pkg.world.dev/world-engine/evm/x/namespace/types"
 	shardtypes "pkg.world.dev/world-engine/evm/x/shard/types"
+	"pkg.world.dev/world-engine/rift/credentials"
+	shard "pkg.world.dev/world-engine/rift/shard/v2"
 )
 
 type EVM struct {
@@ -26,4 +28,19 @@ func NewEVMClient(t *testing.T) *EVM {
 		Bank:      banktypes.NewQueryClient(cc),
 		Namespace: namespacetypes.NewQueryServiceClient(cc),
 	}
+}
+
+type RiftClient struct {
+	Rift shard.TransactionHandlerClient
+}
+
+func NewRiftClient(t *testing.T) *RiftClient {
+	routerKey := "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ01"
+	cc, err := grpc.Dial(
+		"localhost:9601",
+		grpc.WithPerRPCCredentials(credentials.NewTokenCredential(routerKey)),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	assert.NilError(t, err)
+	return &RiftClient{Rift: shard.NewTransactionHandlerClient(cc)}
 }
