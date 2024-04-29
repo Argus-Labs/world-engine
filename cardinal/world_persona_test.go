53c402092a3e80f48e116114032e4b64e82bb814
Jeremy Randolph <jer@argus.gg>
2024-04-29 15:38:50 -0700

    Add test to make sure persona tags remain registered across restarts

--- cardinal/world_persona_test.go
+++ cardinal/world_persona_test.go
@@ -1,13 +1,20 @@
 package cardinal_test
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 	"time"
 
 	"pkg.world.dev/world-engine/assert"
+	"pkg.world.dev/world-engine/cardinal"
+	"pkg.world.dev/world-engine/cardinal/persona/component"
 	"pkg.world.dev/world-engine/cardinal/persona/msg"
+	"pkg.world.dev/world-engine/cardinal/search"
+	"pkg.world.dev/world-engine/cardinal/search/filter"
 	"pkg.world.dev/world-engine/cardinal/testutils"
+	"pkg.world.dev/world-engine/cardinal/types"
+	"pkg.world.dev/world-engine/cardinal/types/engine"
 	"pkg.world.dev/world-engine/sign"
 )
 
@@ -76,4 +83,83 @@ func TestCreatePersonaSystem_WithNoPersonaTagCreateTxs_TickShouldBeFast(t *testi
 	// Fail this test if the second batch of ticks is more than 5 times slower than the original
 	// batch of ticks. The previously registered persona tags should have no performance impact on these systems.
 	assert.True(t, slowdownRatio < 5, "ticks are much slower when many persona tags have been registered")
+}
+
+func TestCreatePersonaSystem_WhenCardinalIsRestarted_PersonaTagsAreStillRegistered(t *testing.T) {
+	tf := testutils.NewTestFixture(t, nil)
+
+	type countPersonaTagsResult struct {
+		personaTags map[string]bool
+		err         error
+	}
+
+	// This is a buffered channel so that the System that uses it doesn't block. It has to be drained
+	// after the DoTick call.
+	numOfPersonaTags := make(chan countPersonaTagsResult, 1)
+
+	// emitNumberOfPersonaTagsSystem is a system that finds all the registered persona tags and sends them
+	// across a channel.
+	emitNumberOfPersonaTagsSystem := func(wCtx engine.Context) error {
+		result := countPersonaTagsResult{personaTags: map[string]bool{}}
+		err := search.NewSearch().
+			Entity(filter.Exact(filter.Component[component.SignerComponent]())).
+			Each(wCtx, func(id types.EntityID) bool {
+				comp, err := cardinal.GetComponent[component.SignerComponent](wCtx, id)
+				result.err = errors.Join(result.err, err)
+				result.personaTags[comp.PersonaTag] = true
+				return true
+			})
+		result.err = errors.Join(result.err, err)
+		numOfPersonaTags <- result
+		return nil
+	}
+	assert.NilError(t, cardinal.RegisterSystems(tf.World, emitNumberOfPersonaTagsSystem))
+
+	msgType, exists := tf.World.GetMessageByFullName("persona.create-persona")
+	assert.True(t, exists)
+
+	// Register 10 persona tags. None of these should fail.
+	for i := 0; i < 10; i++ {
+		currPersonaTag := fmt.Sprintf("pt%d", i)
+		tf.World.AddTransaction(msgType.ID(), msg.CreatePersona{
+			PersonaTag:    currPersonaTag,
+			SignerAddress: fmt.Sprintf("sa%d", i),
+		}, &sign.Transaction{})
+		tf.DoTick()
+		result := <-numOfPersonaTags
+		assert.NilError(t, result.err)
+		assert.Equal(t, i+1, len(result.personaTags))
+		assert.True(t, result.personaTags[currPersonaTag])
+	}
+	// There should now be 10 persona tags registered.
+
+	// Simulate a cardinal restart by creating a new test fixture with the same redis DB.
+	tf = testutils.NewTestFixture(t, tf.Redis)
+	assert.NilError(t, cardinal.RegisterSystems(tf.World, emitNumberOfPersonaTagsSystem))
+
+	// Make sure there are still 10 persona tags registered.
+	tf.DoTick()
+	result := <-numOfPersonaTags
+	assert.NilError(t, result.err)
+	assert.Equal(t, 10, len(result.personaTags))
+	for i := 0; i < 10; i++ {
+		currPersonaTag := fmt.Sprintf("pt%d", i)
+		assert.True(t, result.personaTags[currPersonaTag])
+	}
+
+	// This persona tag has already been registered, so it should fail to register this time.
+	repeatPersonaTag := "pt5"
+	tf.World.AddTransaction(msgType.ID(), msg.CreatePersona{
+		PersonaTag:    repeatPersonaTag,
+		SignerAddress: "some-sa",
+	}, &sign.Transaction{})
+	tf.DoTick()
+
+	// Make sure the receipt from the previous tick talks about the failed persona tag registration.
+	receipts, err := tf.World.GetTransactionReceiptsForTick(tf.World.CurrentTick() - 1)
+	assert.NilError(t, err)
+	assert.Len(t, receipts, 1)
+	errs := receipts[0].Errs
+	assert.Len(t, errs, 1)
+	assert.ErrorContains(t, errs[0], "persona tag pt5 has already been registered")
 }
