16635c071732dc01fc47f31bdb6e105281bf256d
Brian Yeh <brian@argus.gg>
2024-05-10 14:36:28 -0700

    change GetDebugState to GetState

--- cardinal/server/handler/debug.go
+++ cardinal/server/handler/debug.go
@@ -9,14 +9,14 @@ import (
 
 type DebugStateResponse = types.DebugStateResponse
 
-// GetDebugState godoc
+// GetState godoc
 //
 // @Summary      Retrieves a list of all entities in the game state
 // @Description  Retrieves a list of all entities in the game state
 // @Produce      application/json
 // @Success      200  {object}  DebugStateResponse "List of all entities"
 // @Router       /debug/state [post]
-func GetDebugState(world servertypes.ProviderWorld) func(*fiber.Ctx) error {
+func GetState(world servertypes.ProviderWorld) func(*fiber.Ctx) error {
 	return func(ctx *fiber.Ctx) error {
 		var result DebugStateResponse
 		var eachClosureErr error
