aa1901502a17ded10c5a2770928586b8a3cbddf9
Brian Yeh <brian@argus.gg>
2024-05-14 10:56:40 -0700

    make error handling more simple.

--- cardinal/server/handler/query.go
+++ cardinal/server/handler/query.go
@@ -24,7 +24,7 @@ func PostQuery(world servertypes.ProviderWorld) func(*fiber.Ctx) error {
 	return func(ctx *fiber.Ctx) error {
 		ctx.Set("Content-Type", "application/json")
 		resBz, err := world.HandleQuery(ctx.Params("name"), ctx.Body())
-		if eris.Is(err, &types.QueryNotFoundError{}) {
+		if eris.Is(err, types.ErrQueryNotFound) {
 			return fiber.NewError(fiber.StatusNotFound, "query not found")
 		} else if err != nil {
 			return fiber.NewError(fiber.StatusBadRequest, "encountered an error in query: "+err.Error())
--- cardinal/types/query.go
+++ cardinal/types/query.go
@@ -2,14 +2,4 @@ package types
 
 import "fmt"
 
-type QueryNotFoundError struct {
-	name string
-}
-
-func NewQueryNotFoundError(name string) *QueryNotFoundError {
-	return &QueryNotFoundError{name: name}
-}
-
-func (e *QueryNotFoundError) Error() string {
-	return fmt.Sprintf("could not find query with name: %s", e.name)
-}
+var ErrQueryNotFound = fmt.Errorf("query not found")
