872e04c6ad39b11ac1269e493713a7e66d459784
Brian Yeh <brian@argus.gg>
2024-05-31 16:34:57 -0700

    update docs.

--- cardinal/gamestate/doc.go
+++ cardinal/gamestate/doc.go
@@ -1,5 +1,5 @@
 /*
-Package ecb allows for buffering of state changes to the ECS dbStorage layer, and either committing those changes
+Package gamestate allows for buffering of state changes to the ECS dbStorage layer, and either committing those changes
 in an atomic Redis transaction, or discarding the changes. In either case, the underlying Redis DB is never in an
 intermediate state.
 
