971a1bb8836284d15a1009f87cc2d2d98eb17dc1
Scott <scott@argus.gg>
2026-07-28 16:58:45 -0700

    feat(cardinal): allow private system state

--- pkg/cardinal/system.go
+++ pkg/cardinal/system.go
@@ -79,9 +79,13 @@ func initSystemFields[T any](state *T, world *World) error {
 		field := value.Field(i)
 		fieldType := value.Type().Field(i)
 
-		// If the field is not exported, return an error.
-		if !field.CanAddr() {
-			return eris.Errorf("field %s must be exported", fieldType.Name)
+		// Ignore private implementation state, but keep private Cardinal dependencies
+		// as fail-fast configuration errors.
+		if !fieldType.IsExported() {
+			if field.Addr().Type().Implements(reflect.TypeFor[systemField]()) {
+				return eris.Errorf("field %s must be exported", fieldType.Name)
+			}
+			continue
 		}
 
 		fieldInstance := field.Addr().Interface()
